2a52c62957fc35c9645df31f3b422b891dd319ed
KostiaTrepyk <137499840+KostiaTrepyk@users.noreply.github.com>
2024-02-17 03:05:58 +0100

    PointOrb color

--- src/objects/pointOrb/PoinOrb.model.ts
+++ src/objects/pointOrb/PoinOrb.model.ts
@@ -3,7 +3,8 @@ import { Position } from "../../core/types/Position";
 export const RenderPointOrb = (
   ctx: CanvasRenderingContext2D,
   position: Position,
-  size: number
+  size: number,
+  color: string
 ) => {
   /* const randomColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${
     Math.random() * 255
@@ -11,6 +12,6 @@ export const RenderPointOrb = (
 
   ctx.beginPath();
   ctx.arc(position.x, position.y, size / 2, 0, 2 * Math.PI);
-  ctx.fillStyle = "#6b6";
+  ctx.fillStyle = color;
   ctx.fill();
 };
--- src/objects/pointOrb/PointOrb.ts
+++ src/objects/pointOrb/PointOrb.ts
@@ -3,11 +3,14 @@ import { gameObjectManager } from "../../core/global";
 import { Position } from "../../core/types/Position";
 import { RenderPointOrb } from "./PoinOrb.model";
 
-const pointOrbSize: number = 20;
+const pointOrbSize: number = 30;
+const pointOrbColors = ["pink", "lightgreen", "#fa7", "lightblue"] as const;
 
 export class PointOrb extends GameObject<"circle"> {
+  private color: string;
   constructor(startPosition: Position) {
     super(startPosition, { shape: "circle", size: pointOrbSize });
+    this.color = pointOrbColors[Math.round(Math.random() * pointOrbColors.length)];
   }
 
   public override create(): void {
@@ -19,6 +22,6 @@ export class PointOrb extends GameObject<"circle"> {
   }
 
   public override onRender(ctx: CanvasRenderingContext2D): void {
-    RenderPointOrb(ctx, this.position, this.objectModel.size);
+    RenderPointOrb(ctx, this.position, this.objectModel.size, this.color);
   }
 }
